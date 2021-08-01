922a49aef0c52ddb50c2f95af5d4fe7746341259
Jeevan Chhajed <66773723+Jeevan2000@users.noreply.github.com>
2021-08-01 12:27:42 +0530

    Update urls.py

--- CowinSlots/urls.py
+++ CowinSlots/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import url
 from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('',include('vaccine.urls')),
